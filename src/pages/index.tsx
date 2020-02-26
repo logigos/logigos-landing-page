ad24dd020a2350d376384780574dd46ba8429a08
poonsht <poon_arsene_lupin@hotmail.com>
2020-02-26 22:55:03 +0700

    [+] fix : test commit for check ci/cd

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,8 +10,7 @@ const IndexPage = () => {
         <Layout>
             <SEO title="Home" />
             <h1>Hi people</h1>
-            <p>Welcome to your new Gatsby site.</p>
-            <p>Now go build something great.</p>
+            <p>Welcome to Logigos Website</p>
             <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
                 <Image />
             </div>
