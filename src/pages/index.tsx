2ce59cc05a19dba73b1ad067ca25c91750d7f91e
poonsht <poon_arsene_lupin@hotmail.com>
2020-03-02 18:42:40 +0700

    [+] add : assets submodule in bitbucket

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,12 +3,14 @@ import React from 'react'
 import Layout from '../components/common/Layout'
 import SEO from '../components/common/seo'
 import Header from '../components/common/Header'
+import Img from '../images/assets/img.png'
 
 const IndexPage = () => {
     return (
         <Layout>
             <SEO title="Home" />
             <Header />
+            <img src={Img} width="50px" />
         </Layout>
     )
 }
