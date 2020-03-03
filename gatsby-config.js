d1201ba094cc71b29dd27c4be4fd68e8f0793bef
poonsht <poon_arsene_lupin@hotmail.com>
2020-03-03 18:41:29 +0700

    [+] add : install firebase SDK for plug on Google analytics tool.

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,6 +29,22 @@ module.exports = {
                 icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
             },
         },
+        {
+            resolve: 'gatsby-plugin-firebase',
+            options: {
+                credentials: {
+                    apiKey: process.env.GATSBY_FIREBASE_API_KEY,
+                    authDomain: process.env.GATSBY_FIREBASE_AUTH_DOMAIN,
+                    databaseURL: process.env.GATSBY_FIREBASE_DATABASE_URL,
+                    projectId: process.env.GATSBY_FIREBASE_PROJECT_ID,
+                    storageBucket: process.env.GATSBY_FIREBASE_STORAGE_BUCKET,
+                    messagingSenderId:
+                        process.env.GATSBY_FIREBASE_MESSAGING_SENDER_ID,
+                    appId: process.env.GATSBY_FIREBASE_APP_ID,
+                    measurementId: process.env.GATSBY_FIREBASE_MEASUREMENT_ID,
+                },
+            },
+        },
 
         // this (optional) plugin enables Progressive Web App + Offline functionality
         // To learn more, visit: https://gatsby.dev/offline
