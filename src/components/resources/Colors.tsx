ab5b0234279fcc48332e8d610b03fe3b23556405
poonsht <poon_arsene_lupin@hotmail.com>
2020-03-07 17:19:30 +0700

    [+] add : colors on design system

--- src/components/resources/Colors.tsx
+++ src/components/resources/Colors.tsx
@@ -1,3 +1,12 @@
 export const colors: Object = {
-    text: '#0000',
+    base_red: '#ce8d80',
+    base_blue: '#545c73',
+    black_80: 'rgba(0, 0, 0, 0.8)',
+    base_red_10: 'rgba(206, 141, 128, 0.1)',
+    base_blue_10: 'rgba(84, 92, 115, 0.1)',
+    black_60: 'rgba(0, 0, 0, 0.6)',
+    black_40: 'rgba(0, 0, 0, 0.4)',
+    black_10: 'rgba(0, 0, 0, 0.1)',
+    base_red_bold: '#c67b6c',
+    base_blue_bold: '#495064',
 }
